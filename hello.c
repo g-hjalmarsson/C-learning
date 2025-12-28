1c80c05f6fdfcbe9edbb22587f918268c8105d2b
Gustav <gustav.hjalmarsson1@outlook.com>
2025-12-28 15:12:29 +0100

    Initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    //This is a variable in c, int is for the type. Int only displays whole numbers. 
+    int age = 25;
+    int year = 2025;
+
+    //%d This is used to insert varibles into strings.     
+    printf("You are %d years old\n", age);
+    printf("The year is %d\n", year);
+
+    //A float can diplay numbers with decimal points.
+    float gpa = 3.9;
+
+    printf("Your gpa is %f", gpa);
+
+    return 0;
+}
